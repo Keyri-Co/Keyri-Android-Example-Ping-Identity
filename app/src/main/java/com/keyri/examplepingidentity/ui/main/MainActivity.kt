3839bf4af0d6d022bc183c588422741002ddd249
kuliahin.a <kuliahin.a@abcloudz.com>
2022-06-26 17:12:11 +0300

    Added Oauth flow, extend payload

--- app/src/main/java/com/keyri/examplepingidentity/ui/main/MainActivity.kt
+++ app/src/main/java/com/keyri/examplepingidentity/ui/main/MainActivity.kt
@@ -9,13 +9,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.snackbar.Snackbar
 import com.keyri.examplepingidentity.R
+import com.keyri.examplepingidentity.data.AccessToken
 import com.keyri.examplepingidentity.data.Consts
+import com.keyri.examplepingidentity.data.create_user.response.UserResponse
 import com.keyri.examplepingidentity.databinding.ActivityMainBinding
+import com.keyrico.keyrisdk.Keyri
 import com.keyrico.keyrisdk.ui.auth.AuthWithScannerActivity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import org.json.JSONObject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -26,9 +31,6 @@ class MainActivity : AppCompatActivity() {
 
             showMessage(findViewById(R.id.llRoot), text)
         }
-
-    // TODO 1. Add authorization
-    // TODO 2. Change README.md
 
     private val viewModel by viewModel<MainViewModel>()
 
@@ -49,11 +51,14 @@ class MainActivity : AppCompatActivity() {
         intent.dataString?.let { Uri.parse(it).getQueryParameter(Consts.CODE) }?.let { code ->
             val clientId = getString(R.string.client_id)
             val redirectUri = getString(R.string.redirect_uri)
+            val userInfoUri = getString(R.string.user_info_uri)
 
             lifecycleScope.launch(Dispatchers.IO) {
                 viewModel.proceedWithCode(code, clientId, redirectUri)
-                    // TODO Handle result
-                    .collect()
+                    .collectLatest { accessToken ->
+                        viewModel.getUserInfo(userInfoUri, accessToken)
+                            .collectLatest { keyriAuth(accessToken, it) }
+                    }
             }
         }
     }
@@ -75,6 +80,65 @@ class MainActivity : AppCompatActivity() {
 
                     startActivity(Intent(Intent.ACTION_VIEW, authorizationUrl))
                 }.collect()
+        }
+    }
+
+    private fun keyriAuth(accessToken: AccessToken, user: UserResponse) {
+        val environmentId = getString(R.string.environment_id)
+
+        val email = user.email
+        val keyri = Keyri()
+
+        val associationKey = keyri.getAssociationKey(email)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            viewModel.saveSignaturePublicKey(
+                accessToken.accessToken,
+                environmentId,
+                user.id,
+                associationKey
+            )
+                .collectLatest {
+                    val userData = JSONObject().apply {
+                        put("id", user.id)
+                        put("username", user.username)
+                        put("email", user.email)
+                        put("createdAt", user.createdAt)
+                        put("updatedAt", user.updatedAt)
+                        put("enabled", user.enabled)
+                    }
+
+                    val tokenData = JSONObject().apply {
+                        put("accessToken", accessToken.accessToken)
+                        put("idToken", accessToken.idToken)
+                        put("tokenType", accessToken.tokenType)
+                        put("expiresIn", accessToken.expiresIn)
+                        put("scope", accessToken.scope)
+                    }
+
+                    val data = JSONObject().apply {
+                        put("user", userData)
+                        put("token", tokenData)
+                    }
+
+                    val signingData = JSONObject().apply {
+                        put("timestamp", System.currentTimeMillis())
+                        put("email", user.email)
+                        put("id", user.id)
+                    }.toString()
+
+                    val signature = keyri.getUserSignature(email, signingData)
+
+                    val payload = JSONObject().apply {
+                        put("data", data)
+                        put("signingData", signingData)
+                        put("userSignature", signature) // Optional
+                        put("associationKey", associationKey) // Optional
+                    }.toString()
+
+                    // Public user ID (email) is optional
+                    keyriAuth(email, payload)
+                }
         }
     }
 
--- app/src/main/java/com/keyri/examplepingidentity/ui/main/MainViewModel.kt
+++ app/src/main/java/com/keyri/examplepingidentity/ui/main/MainViewModel.kt
@@ -4,13 +4,12 @@ import androidx.lifecycle.ViewModel
 import com.keyri.examplepingidentity.data.AccessToken
 import com.keyri.examplepingidentity.data.Consts
 import com.keyri.examplepingidentity.data.ServerConfig
+import com.keyri.examplepingidentity.data.create_user.response.UserResponse
 import com.keyri.examplepingidentity.repository.auth.AuthRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.onEach
 
 class MainViewModel(private val authRepository: AuthRepository) : ViewModel() {
-
-    private var serverConfig: ServerConfig? = null
 
     fun authorize(discoveryUri: String, environmentId: String): Flow<ServerConfig> {
         val url = String.format(discoveryUri, environmentId)
@@ -30,5 +29,24 @@ class MainViewModel(private val authRepository: AuthRepository) : ViewModel() {
             grantType = Consts.AUTHORIZATION_CODE,
             redirectUri = redirectUri
         )
+    }
+
+    fun getUserInfo(userInfoEndpoint: String, accessToken: AccessToken): Flow<UserResponse> {
+        val bearerToken = accessToken.tokenType + " " + accessToken.accessToken
+
+        return authRepository.getUserInfo(userInfoEndpoint, bearerToken)
+    }
+
+    fun saveSignaturePublicKey(
+        bearerToken: String,
+        environmentId: String,
+        userId: String,
+        publicKey: String
+    ): Flow<String> {
+        return authRepository.saveSignaturePublicKey(bearerToken, environmentId, userId, publicKey)
+    }
+
+    companion object {
+        private var serverConfig: ServerConfig? = null
     }
 }
